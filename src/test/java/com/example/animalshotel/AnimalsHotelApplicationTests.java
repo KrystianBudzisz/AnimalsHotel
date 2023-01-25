4cb2b7484ecfa34c8cf5089893c087a897a2a1e5
Budzisz <krystianbudzisz1@gmail.com>
2023-01-25 18:46:50 +0100

    first commit

---
+++ src/test/java/com/example/animalshotel/AnimalsHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalshotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
